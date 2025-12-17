26dbb29ab3842b3c09b0221ea3f9e8f2541278e3
Rafael Martinez Perez <ramarti2@c2r9s4.42malaga.com>
2025-12-17 17:01:49 +0100

    a

--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -1,12 +1,15 @@
-
+#include <iostream>
 
 class Contact
 {
+	
 	public:
 		char *firstname;
 		char *lastname;
 		char *nickname;
 		char *number;
-		Contact(char *firstname, char *lastname, char *nickname, char *number);
+		char *secret;
+		Contact(void);
+		Contact(char *firstname, char *lastname, char *nickname, char *number, char *secret);
 		~Contact(void);
 };
