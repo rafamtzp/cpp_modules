ff3a4405f507cba8f3c7d7795466553e93743efe
rafamtzp <rafamtz2001@gmail.com>
2025-12-19 18:32:17 +0100

    Haciendo tabla

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -1,45 +1,60 @@
 #include "PhoneBook.hpp"
+#include <iomanip>
 
 
 PhoneBook::PhoneBook(void)
 {
 	PhoneBook::index = 0;
-	std::cout << "Phonebook constructor called" << std::endl;
+	//std::cout << "Phonebook constructor called" << std::endl;
 	return ;
 }
 
 PhoneBook::~PhoneBook(void)
 {
-	std::cout << "Phonebook destructor called" << std::endl;
+	//std::cout << "Phonebook destructor called" << std::endl;
 	return ;
 }
 
-void PhoneBook::add_contact(Contact contacts[], int index)
+void PhoneBook::add_contact(void)
 {
 	std::cout << "Please enter their first name: ";
-	std::cin >> contacts[index].firstname;
+	getline(std::cin, this->contacts[this->index].firstname);
 	std::cout << "Please enter their last name: ";
-	std::cin >> contacts[index].lastname;
+	getline(std::cin, this->contacts[this->index].lastname);
 	std::cout << "Please enter their nickname: ";
-	std::cin >> contacts[index].nickname;
+	getline(std::cin, this->contacts[this->index].nickname);
 	std::cout << "Please enter their phone number: ";
-	std::cin >> contacts[index].number;
+	getline(std::cin, this->contacts[this->index].number);
 	std::cout << "Please enter their darkest secret (shh): ";
-	std::cin >> contacts[index].secret;
+	getline(std::cin, this->contacts[this->index].secret);
 	this->index++;
-	if (index == 8)
+	if (this->index == 8)
 		this->index = 0;
 }
 
-void PhoneBook::search_contact(Contact contacts[])
+void PhoneBook::search_contact(void)
 {
-	char *index = "0";
+	std::string index = "0";
 
-	std::cout << "* table *" << std::endl;
-	while (atoi(index) == 0)
+	std::cout
+	<< std::string('-', 45) << std::endl
+	<< std::right << "|" << std::setw(10) << "Index"
+	<< std::right << "|" << std::setw(10) << "First Name"
+	<< std::right << "|" << std::setw(10) << "Last Name"
+	<< std::right << "|" << std::setw(10) << "Nickname"
+	<< "|" << std::endl << std::string(45, '-') << std::endl;
+	for (int i = 0; i < 8; i++)
+	{
+		if (this->contacts[i].has_data())
+		{
+			std::cout
+			<< std::right << "|" << std::
+		}
+	}
+	while (index == "0")
 	{
 		std::cout << "Please enter an index: ";
-		std::cin >> index;
+		std::getline(std::cin, index);
 	}
 	std::cout << "Index entered: " << index << std::endl;
 	
