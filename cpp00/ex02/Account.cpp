5f75a04e8f0a4a6cc13d08063bc80b910ba9fb0a
rafamtzp <rafamtz2001@gmail.com>
2026-01-16 16:38:19 +0100

    getting timestamp

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,13 +1,29 @@
 #include "Account.hpp"
 #include <iostream>
+#include <ctime>
+#include <cstdio>
+#include <string>
+
+std::string get_time_stamp(void)
+{
+	time_t now = std::time(NULL);
+	char buffer[20];
+	std::strftime(buffer, sizeof(buffer), "%Y%m%d_%H%M%S", std::localtime(&now));
+	return std::string(buffer);
+}
 
 Account::Account(int initial_deposit)
 {
 	static int i = 0;
+	time_t timestamp;
+	char buffer[16];
 
 	Account::_accountIndex = 0;
 	i++;
 	Account::_amount = initial_deposit;
-	std::cout << "[19920104_091532] index:0;amount:42;created"
+	time(&timestamp);
+	std::strftime(buffer, sizeof(buffer), "%Y%m%d_%H%M%S", std::localtime(&timestamp));
+	std::cout << "[" << buffer << "]" "index:"<< 0 << ";amount:" << 42 << ";created" << std::endl;
 	return ;
 }
+
