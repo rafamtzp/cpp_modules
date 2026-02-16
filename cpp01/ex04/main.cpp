354054ff4aae4a31edb5e52a0177a2c2c0f64453
Rafael Martinez Perez <ramarti2@c3r3s4.42malaga.com>
2026-02-16 16:50:11 +0100

    starting cpp03

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -10,21 +10,39 @@
 #include <iostream>
 #include <fstream>
 
+std::string	line_replaceAll(std::string line, std::string s1, std::string s2)
+{
+	int pos = line.find(s1);
+	while (pos != std::string::npos)
+	{
+		line.erase(pos, s1.length());
+		line.insert(pos, s2);
+		pos = line.find(s1, pos);
+	}
+	return (line);
+}
+
 int main(int argc, char **argv)
 {
 	if (argc != 4)
+	{
+		std::cerr << "Usage: ./main <file name> <string to find> <replacement>"
+		<< std::endl;
 		return 1;
+	}
+
 	std::string filename(argv[1]);
 	std::ifstream file(filename);
-	std::ofstream file_copy(filename.append(".replace"));
-	std::string strread;
-	std::string s1(argv[2]);
-	std::string s2(argv[3]);
-
-	while (file >> strread)
+	if (!file)
 	{
-		if (strread ==  s1)
-			file_copy << 
-		std::cout << strread << std::endl;
+		std::cerr << "Could not open file (does not exist or no permission)" << std::endl;
+		return (1);
+	}
+
+	std::ofstream file_copy(filename.append(".replace"));
+	std::string line;
+	while (std::getline(file, line))
+	{
+		file_copy << line_replaceAll(line, argv[2], argv[3]) << std::endl;
 	}
 }
