7b248bbec1d11877beccd743cc6fcd5cd3a57a41
Rafael <rafamtz2001@gmail.com>
2026-02-23 13:54:51 +0100

    Done with cpp02

--- cpp02/ex03/Point.hpp
+++ cpp02/ex03/Point.hpp
@@ -5,16 +5,21 @@ class Point
     private:
         const Fixed x;
         const Fixed y;
+        Point&  operator=( const Point& other );
 
     public:
     // Constructors and destructors
         Point( void );
         Point( const float f1, const float f2 );
         Point( const Point& other );
+        Point( const Fixed x, const Fixed y);
         ~Point( void );
-    // Operators
-        Point&  operator=( const Point& other );
+
+    // Getters
         const Fixed getx( void ) const;
         const Fixed gety( void ) const;
+    
+    // Operators
+    Point   operator-( const Point& other );
 };
 
