85e5df407fe05d4e318d4ece62a77dcebd145dab
rafamtzp <rafamtz2001@gmail.com>
2026-02-25 16:34:34 +0100

    car

---
+++ cpp04/ex00/Animal.cpp
@@ -0,0 +1,23 @@
+#include "Animal.hpp"
+
+Animal::Animal( void ) : type("Animal")
+{
+	std::cout <<
+	"Animal created"
+	<<std::endl;
+}
+
+Animal::Animal( const Animal& other ) : type(other.type)
+{
+	std::cout << "Animal Copy constructor called" << std::endl;
+}
+Animal::~Animal( void )
+{
+	std::cout <<"Animal destroyed"<<std::endl;
+}
+Animal& Animal::operator=( const Animal& other )
+{
+	if (this != &other)
+		this->type = other.type;
+	return (*this);
+}
