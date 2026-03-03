1340b7e9e39ea1b3ddb74e389a5c107c8832624e
rafamtzp <rafamtz2001@gmail.com>
2026-03-03 18:12:53 +0100

    phonebook fixed

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -13,10 +13,10 @@ Contact::Contact(void)
 
 bool Contact::has_data(void) const
 {
-	if (!this->firstname.empty() || !this->lastname.empty() ||
-		!this->nickname.empty() || !this->number.empty() || !this->secret.empty())
-		return true;
-	return false;
+	if (this->firstname.empty() || this->lastname.empty() ||
+		this->nickname.empty() || this->number.empty() || this->secret.empty())
+		return false;
+	return true;
 }
 
 void Contact::clear(void)
@@ -39,4 +39,47 @@ Contact::~Contact(void)
 {
 	//std::cout << "Contact Destructor called" << std::endl;
 	return ;
+}
+
+bool	Contact::setField(std::string field, std::string input)
+{
+	bool is_valid = false;
+
+	for (int i = 0; i < input.length() && !is_valid; i++) //check nonempty
+	{
+		if (isascii(input[i]) && !isspace(input[i]))
+			is_valid = true;
+	}
+	for (int i = 0; i < input.length() && is_valid; i++) // check all ascii
+	{
+		if (!isascii(input[i]))
+			is_valid = false;
+	}
+	if (is_valid == true)
+	{
+		if (field == "firstname")
+			firstname = input;
+		else if (field == "lastname")
+			lastname = input;
+		else if (field == "nickname")
+			nickname = input;
+		else if (field == "number")
+			number = input;
+		else if (field == "secret")
+			secret = input;
+	}
+	return (is_valid);
+}
+
+std::string	Contact::getField(std::string field)
+{
+	if (field == "firstname")
+		return firstname;
+	else if (field == "lastname")
+		return lastname;
+	else if (field == "nickname")
+		return nickname;
+	else if (field == "number")
+		return number;
+	return secret;
 }
