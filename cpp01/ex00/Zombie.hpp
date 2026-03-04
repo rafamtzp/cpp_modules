4ed15ccdcc37a14a48d835dfabf124c2d70c3ed8
rafamtzp <rafamtz2001@gmail.com>
2026-03-04 17:01:09 +0100

    CPP01 check

--- cpp01/ex00/Zombie.hpp
+++ cpp01/ex00/Zombie.hpp
@@ -1,12 +1,16 @@
-#include <iostream>
 
-class Zombie
-{
-	private:
-		std::string	name;
-	
-	public:
-		Zombie( std::string );
-		~Zombie( void );
-		void announce(void);
-};
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+	#include <iostream>
+
+	class Zombie
+	{
+		private:
+			std::string	name;
+		
+		public:
+			Zombie( std::string );
+			~Zombie( void );
+			void announce(void);
+	};
+#endif
--- cpp01/ex03/HumanA.hpp
+++ cpp01/ex03/HumanA.hpp
@@ -1,15 +1,17 @@
+#ifndef HUMANA_HPP
+#define HUMANA_HPP
+	#include "Weapon.hpp"
+	#include <iostream>
 
-#include "Weapon.hpp"
-#include <iostream>
-
-class	HumanA
-{
-	private:
-		std::string name;
-		Weapon& r_weapon;
-	
-	public:
-		HumanA(std::string name, Weapon& r_weapon);
-		~HumanA();
-		void	attack();
-};
+	class	HumanA
+	{
+		private:
+			std::string name;
+			Weapon& r_weapon;
+		
+		public:
+			HumanA(std::string name, Weapon& r_weapon);
+			~HumanA();
+			void	attack();
+	};
+#endif
