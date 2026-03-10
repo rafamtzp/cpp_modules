f8b7302fd70017d5703018b4334397ed37542d0c
rafamtzp <rafamtz2001@gmail.com>
2026-03-10 13:24:36 +0100

    checked cpp02

--- cpp02/ex03/Point.hpp
+++ cpp02/ex03/Point.hpp
@@ -7,22 +7,22 @@
 		private:
 			const Fixed x;
 			const Fixed y;
-			Point&  operator=( const Point& other );
-
+			
 		public:
-		// Constructors and destructors
+			// Constructors and destructors
 			Point( void );
 			Point( const float f1, const float f2 );
 			Point( const Point& other );
 			Point( const Fixed x, const Fixed y);
 			~Point( void );
 
-		// Getters
+			// Getters
 			const Fixed getx( void ) const;
 			const Fixed gety( void ) const;
-		
-		// Operators
-		Point   operator-( const Point& other );
+			
+			// Operators
+			Point   operator-( const Point& other );
+			Point&  operator=( const Point& other );
 	};
 #endif
 
