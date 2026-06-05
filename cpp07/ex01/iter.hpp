9cf9ee3437ed5662887b93200030fd2a0b3dacae
Rafael <rafamtz2001@gmail.com>
2026-06-05 10:45:38 +0200

    working on cpp07/ex02

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+template <typename T, typename F>
+void    iter(T *addr, const int len, F f)
+{
+    for (int i = 0; i < len; i++)
+        f(addr[i]);
+    return ;
+}
---
+++ cpp07/ex01/main.cpp
@@ -0,0 +1,48 @@
+#include "iter.hpp"
+#include <iostream>
+#include <string>
+
+// int functions
+void addone(int& a)
+{
+    a++;
+}
+
+void printIntConst(const int& a)
+{
+    std::cout << a << " ";
+}
+
+// string functions
+void appendBang(std::string& s)
+{
+    s += "!";
+}
+
+void printStringConst(const std::string& s)
+{
+    std::cout << s << " ";
+}
+
+int main(void)
+{
+    std::cout << "--- Testing standard integer array ---" << std::endl;
+    int arr[3] = {1, 2, 3};
+    iter(arr, 3, addone);
+    iter(arr, 3, printIntConst); 
+    std::cout << std::endl;
+
+    std::cout << "\n--- Testing CONST integer array ---" << std::endl;
+    const int carr[3] = {10, 20, 30};
+    //iter(carr, 3, addone);   // compiler error if uncommented
+    iter(carr, 3, printIntConst);
+    std::cout << std::endl;
+
+    std::cout << "\n--- Testing string array ---" << std::endl;
+    std::string sarr[3] = {"Hello", "42", "World"};
+    iter(sarr, 3, appendBang);
+    iter(sarr, 3, printStringConst);
+    std::cout << std::endl;
+
+    return 0;
+}
