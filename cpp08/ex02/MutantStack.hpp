85b69c087cc28264666a12c2b28807174accf4db
Rafael <rafamtz2001@gmail.com>
2026-06-12 16:55:45 +0200

    cpp08/ex02 compiles

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -2,19 +2,29 @@
 #include <stack>
 
 template <typename T>
-class MutantStack
+class MutantStack : public std::stack<T>
 {
-	private:
-	std::stack<T> stack;
-
 	public:
 	MutantStack();
 	MutantStack(const MutantStack& other);
 	~MutantStack();
-	MutantStack&	operator=(const MutantStack& other);
+	MutantStack<T>&	operator=(const MutantStack& other);
+	
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
-	class iterator
-	{
-		
-	};
+	iterator	begin();
+	const_iterator	begin() const;
+	reverse_iterator rbegin();
+	const_reverse_iterator rbegin() const;
+
+	iterator	end();
+	const_iterator	end() const;
+	reverse_iterator	rend();
+	const_reverse_iterator	rend() const;
+
 };
+
+#include "MutantStack.tpp"
