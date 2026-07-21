9fd9365579a1d76d6f12738c77bcd221c0fd33fd
Rafael <rafamtz2001@gmail.com>
2026-07-21 15:46:13 +0200

    added sortWrapper that prints results cpp09/ex02

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ramarti2 <ramarti2@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 10:54:35 by ramarti2          #+#    #+#             */
-/*   Updated: 2026/07/21 13:00:29 by ramarti2         ###   ########.fr       */
+/*   Updated: 2026/07/21 15:44:24 by ramarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <cerrno>
 #include <vector>
 #include <deque>
+#include <sys/time.h>
 
 int retErr(const char *msg)
 {
@@ -45,32 +46,59 @@ int parseInput(char **argv, C& cont)
         cont.push_back(static_cast<int>(num));
         ct++;
     }
-    std::cout << "The number of words is: " << ct << std::endl;
     return 0;
 }
 
 template <typename C>
-void    printCont(const C& cont)
+void    printCont(const C& cont, bool before)
 {
     typename C::const_iterator end = cont.end();
 
-    std::cout << "Container values: ";
+    if (before)
+        std::cout << "Before: ";
+    else
+        std::cout << "After: ";
     for (typename C::const_iterator it = cont.begin(); it != end; ++it)
     {
         std::cout << *it << " ";
     }
     std::cout << std::endl;
 }
+template <typename C>
+void sortCont(C& cont)
+{
+    (void)cont;
+}
+#include <unistd.h>
 
+void    sortWrapper(std::deque<int>& deque, std::vector<int>& vec)
+{
+    struct timeval t1, t2;
+
+    printCont(vec, true);
+    gettimeofday(&t1, NULL);
+    sleep(1); // sort vec
+    sortCont(vec);
+    gettimeofday(&t2, NULL);
+    printCont(vec, false);
+    std::cout << "Time taken to process a range of " << vec.size() << " elements with std::vector<int> : " 
+     << (t2.tv_usec - t1.tv_usec) << " us" << std::endl;
+
+    gettimeofday(&t1, NULL);
+    sortCont(deque);
+    gettimeofday(&t2, NULL);
+    std::cout << "Time taken to process a range of " << deque.size() << " elements with std::deque<int> : " 
+     << (t2.tv_usec - t1.tv_usec) << " us" << std::endl;
+}
 
 int main(int argc, char **argv)
 {
     std::deque<int>  maind;
     std::vector<int> mainv;
     (void)argc;
+
     if (parseInput(argv, maind) == 1 || parseInput(argv, mainv))
         return 1;
-    printCont(maind);
-    printCont(mainv);
+    sortWrapper(maind, mainv);
     return 0;
 }
