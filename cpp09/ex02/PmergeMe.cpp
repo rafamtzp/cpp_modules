d159083d28fba7d37c15ae8c68e690ac7a4c15a4
rafamtzp <rafamtz2001@gmail.com>
2026-07-22 16:38:43 +0200

    idk what I'm doing...

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,21 +6,22 @@
 /*   By: ramarti2 <ramarti2@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 14:26:34 by ramarti2          #+#    #+#             */
-/*   Updated: 2026/07/22 15:57:13 by ramarti2         ###   ########.fr       */
+/*   Updated: 2026/07/22 16:21:07 by ramarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-template <typename C>
-void    sortCont(C& cont, int pairsize = 1)
+template <typename C, typename T>
+void    sortCont(C& cont, unsigned int pairsize = 1)
 {
 	// if can't split into pairs, return
 	if (cont.size() / pairsize <= 1)
 		return ;
     // split into pairs and swap them
-	C<std::pair
+	C pairs
 	
 	// call sortCont(cont, pairsize * 2)
+	sortCont(cont, pairsize * 2);
 
 	// Take out b's from main container to create pend
 
